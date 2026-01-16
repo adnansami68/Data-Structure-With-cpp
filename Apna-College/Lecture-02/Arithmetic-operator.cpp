d601f64f3008ff02cae45f576ed4eab678da50c1
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-16 22:15:32 +0600

    Create Arithmetic-operator.cpp

---
+++ Apna-College/Lecture-02/Arithmetic-operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a =10 , b=5; //assigning values to variables
+    
+    //int sum=a+b;
+    //cout<<sum<<endl;
+
+    cout<<"Sum= " <<(a+b)<<endl;
+    cout<<"Difference= " <<(a-b)<<endl;
+    cout<<"Product= " <<(a*b)<<endl;
+    cout<<"Quotient= " <<(a/b)<<endl;
+    cout<<"Remainder= " <<(a%b)<<endl;
+
+    return 0;
+}
+
+
+
