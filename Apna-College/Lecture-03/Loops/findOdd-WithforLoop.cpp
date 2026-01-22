44700420735cfde6f0948b35dc7dbdd0bcfc8c56
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-22 21:17:22 +0600

    Create findOdd-WithforLoop.cpp

---
+++ Apna-College/Lecture-03/Loops/findOdd-WithforLoop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=20;
+
+    for(int i=1; i<=n;i++){
+        if(i%2!=0){
+            cout<<i<< "  " ;
+        }
+    }
+    return 0;
+}
