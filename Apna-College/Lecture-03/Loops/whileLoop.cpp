897414ebc6d35d75cf2355e3c1c00ecadef6da9e
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-22 19:20:00 +0600

    Create whileLoop.cpp
    Qs: Print numbers 1 to 5

---
+++ Apna-College/Lecture-03/Loops/whileLoop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n , i=1;
+    cout<<"Enter Number: ";
+    cin>>n;
+
+    while(i<=n){   //Qs:Print numbers 1 to 5
+        cout<<i<< "  ";
+        i++;
+    }
+    
+   return 0;
+
+}
+
+
