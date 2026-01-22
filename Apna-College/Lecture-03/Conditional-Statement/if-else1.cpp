91f55a16e946a84cfe4e15626b4939fa4e765e0c
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-22 19:14:23 +0600

    Create if-else1.cpp

---
+++ Apna-College/Lecture-03/Conditional-Statement/if-else1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"Enter Number:"<<endl;
+    
+    cin>>n;
+
+    if(n>=0){
+        
+        cout<<"n is Positive number"<<endl;
+
+    } else{
+
+        cout<<"n is Negative number"<<endl;
+    }
+    
+    return 0;
+}
