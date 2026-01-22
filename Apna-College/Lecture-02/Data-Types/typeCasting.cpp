8171a6ef97f002e5d158c852657769c183fef340
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-22 19:34:10 +0600

    Create typeCasting.cpp

---
+++ Apna-College/Lecture-02/Data-Types/typeCasting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double price=100.99;
+
+    int newPrice= price; //implicit type casting
+    cout<<newPrice<<endl;
+
+    return 0;
+
+}
