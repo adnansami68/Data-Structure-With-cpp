0508b1eccf4d613525ca9ba4598c22e7d299b180
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-22 19:26:43 +0600

    Create forLoop.cpp

---
+++ Apna-College/Lecture-03/Loops/forLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=20;
+    int sum=0;
+
+    for(int i=1 ; i<=n ; i++){
+        sum+=i;
+    }
+    cout<<"Sum = "<<sum<<endl;
+
+    return 0;
+}
