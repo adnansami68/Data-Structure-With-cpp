112ab0084931e7ca0ef77e3355be0c85be19c6b0
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-29 14:37:34 +0600

    Add C++ program to count element frequencies

---
+++ CSE-221-and-222/Lab-Practice/14-01-2026/practice-4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter array size: ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter elements: ";
+    for(int i=0 ; i<n ;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<n-1 ; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    cout<<"Frequency of Element"<<endl;
+    int count=1;
+    for(int i=0;i<n ; i++){
+        if(i==n-1 || arr[i]!=arr[i+1]){
+            cout<<arr[i]<<"->"<<count<<" Times"<<endl;
+            count=1;
+        }
+        else{
+            count++;
+        }
+    }
+
+    return 0;
+}
